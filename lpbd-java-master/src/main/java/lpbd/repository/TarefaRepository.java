c0ff28ddaa2c4a62c05c472ea37cda7b20695bc3
juliosrangel <julio.cesar.s.rangel@gmail.com>
2020-06-15 20:00:31 -0300

    atualização service tarefa

---
+++ lpbd-java-master/src/main/java/lpbd/repository/TarefaRepository.java
@@ -0,0 +1,9 @@
+package lpbd.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import lpbd.model.Tarefa;
+
+public interface TarefaRepository extends CrudRepository<Tarefa, Integer>{
+
+}
---
+++ lpbd-java-master/src/main/java/lpbd/response/ObjectUser.java
@@ -0,0 +1,67 @@
+package lpbd.response;
+
+import com.google.gson.Gson;
+
+public class ObjectUser {
+	private Integer id;
+	private String nome;
+	private String sobrenome;
+	private Long dataNascimento;
+	private String cpf;
+	private String telefone;
+	
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSobrenome() {
+		return sobrenome;
+	}
+
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+
+	public Long getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(Long dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	@Override
+	public String toString() {
+		return new Gson().toJson(this);
+	}
+	
+}
