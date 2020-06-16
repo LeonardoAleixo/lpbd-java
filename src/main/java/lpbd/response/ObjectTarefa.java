44f2202c6e0b4c630ca088f680ba55e8d2c1c9a6
sandrotoline <sandro.toline@gmail.com>
2020-06-15 21:17:44 -0300

    implementação do tarefa rest e criação de objeto de retorno

---
+++ src/main/java/lpbd/response/ObjectTarefa.java
@@ -0,0 +1,77 @@
+package lpbd.response;
+
+import com.google.gson.Gson;
+
+public class ObjectTarefa {
+	private Integer idTarefa;
+	private Long dataFim;
+	private Integer usuario;
+
+	private Integer importancia;
+
+	private Long dataPrevistaFim;
+	private String titulo;
+	private Long dataInicio;
+
+	public Integer getIdTarefa() {
+		return idTarefa;
+	}
+
+	public void setIdTarefa(Integer idTarefa) {
+		this.idTarefa = idTarefa;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public Long getDataInicio() {
+		return dataInicio;
+	}
+
+	public void setDataInicio(Long dataInicio) {
+		this.dataInicio = dataInicio;
+	}
+
+	public Long getDataFim() {
+		return dataFim;
+	}
+
+	public void setDataFim(Long dataFim) {
+		this.dataFim = dataFim;
+	}
+
+	public Integer getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Integer usuario) {
+		this.usuario = usuario;
+	}
+
+	public Integer getImportancia() {
+		return importancia;
+	}
+
+	public void setImportancia(Integer importancia) {
+		this.importancia = importancia;
+	}
+
+	public Long getDataPrevistaFim() {
+		return dataPrevistaFim;
+	}
+
+	public void setDataPrevistaFim(Long dataPrevistaFim) {
+		this.dataPrevistaFim = dataPrevistaFim;
+	}
+
+	@Override
+	public String toString() {
+		return new Gson().toJson(this);
+	}
+
+}
---
+++ src/main/java/lpbd/rest/TarefaRest.java
@@ -0,0 +1,69 @@
+package lpbd.rest;
+
+import java.util.Date;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import lpbd.response.ObjectTarefa;
+import lpbd.service.TarefaService;
+
+@RestController
+@Validated
+@RequestMapping("/lpbd/tarefa")
+public class TarefaRest {
+	@Autowired
+	private TarefaService userService;
+
+	@GetMapping("/get/id/{id}/")
+	@CrossOrigin
+	ResponseEntity<String> getBbyId(@PathVariable Integer id) {
+		String response = null;
+		try {
+			response = userService.encontrarPorId(id).toString();
+		} catch (Exception e) {
+			return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
+		}
+		return new ResponseEntity<String>(response, HttpStatus.OK);
+	}
+	
+	
+	@GetMapping("/get/all/")
+	@CrossOrigin(origins = "*", maxAge = 3600)
+	ResponseEntity<String> getAll() {
+		String response = null;
+		try {
+			response = userService.encontrarPorPeriodo( new Date(), new Date()).toString();
+		} catch (Exception e) {
+			return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
+		}
+		return new ResponseEntity<String>(response, HttpStatus.OK);
+	}
+
+	
+	@PostMapping("/save/")
+	@ResponseStatus(HttpStatus.CREATED)
+	@CrossOrigin
+	ResponseEntity<String> save(@Valid @RequestBody ObjectTarefa objectTarefa) {
+		String response = "";
+		try {
+			response = userService.salvar(objectTarefa).toString();
+		} catch (Exception e) {
+			return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
+		}
+		return new ResponseEntity<String>(response, HttpStatus.OK);
+	}
+
+}
