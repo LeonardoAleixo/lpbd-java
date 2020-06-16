e8c3f2956759831ddee0fb76f8f00add00bfb7e3
valdirEva <valdirevaristo.silva@gmail.com>
2020-06-15 21:28:26 -0300

    correção de arquivos não configurados do projeto

--- lpbd-java-master/src/main/java/lpbd/repository/TarefaRepository.java
+++
@@ -1,9 +0,0 @@
-package lpbd.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import lpbd.model.Tarefa;
-
-public interface TarefaRepository extends CrudRepository<Tarefa, Integer>{
-
-}
--- lpbd-java-master/src/main/java/lpbd/service/PessoaFisicaValidator.java
+++
@@ -1,9 +0,0 @@
-package lpbd.service;
-
-/*
- * Alterado LPBD - Douglas Nishiama - 25/05/2019 
- * Inteface para validar o cpf do usuário e outras funcionalidades de pessoa física
- */
-public interface PessoaFisicaValidator {
-	public boolean validarCpf(String cpf);
-}
